73bb3dd1cce169c7eaceb7d14c1ee91b67461f70
Agustin Flores Lopez <afl231001@gmail.com>
2026-07-01 01:16:12 -0600

    Revert "cambios espero"
    This reverts commit 5b3ed8aac27e8135e664be008f543aaa13574d08.

--- app/modules/snmp_utils.py
+++ app/modules/snmp_utils.py
@@ -1,9 +1,4 @@
-from pysnmp.hlapi import (
-    SnmpEngine, CommunityData, UsmUserData, UdpTransportTarget, ContextData,
-    ObjectType, ObjectIdentity, OctetString,
-    getCmd, setCmd,
-    usmHMACSHAAuthProtocol, usmDESPrivProtocol
-)
+from pysnmp.hlapi import *
 
 def get_auth_data(creds):
     if creds.get('snmp_version') == '2c':
@@ -14,7 +9,7 @@ def get_auth_data(creds):
             authKey=creds.get('v3_auth'),
             privKey=creds.get('v3_priv'),
             authProtocol=usmHMACSHAAuthProtocol,
-            privProtocol=usmDESPrivProtocol
+            privProtocol=usmAesCfb128Protocol
         )
 
 def snmp_get(ip, oid, creds):
